3763e916382b854a002974edaeacc316b1189767
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-08-04 10:16:20 -0700

    user model created

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// email validation function
+// https://thewebdev.info/2022/03/16/how-to-validate-email-syntax-with-mongoose/
+const validateEmail = (email) => {
+  const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      max_length: 50,
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [thoughtSchema],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
